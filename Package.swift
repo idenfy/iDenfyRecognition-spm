d4a438b3540cc8d1ecb1a1b754225730b925c2f5
viktasidenfy <78145743+viktasidenfy@users.noreply.github.com>
2026-09-17 10:30:47 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ enum Checksums {
 
 let package = Package(
     name: "iDenfyRecognition",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "iDenfyRecognition-Dynamic",
